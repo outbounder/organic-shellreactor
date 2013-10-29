ea064db0dffcf4cb273c7e24dfa9fd2364dff1c3
Boris Filipov <obiwonn@gmail.com>
2013-10-29 23:49:41 +0200

    reactions with complex next support

--- index.js
+++ index.js
@@ -1,23 +1,135 @@
 var shelljs = require("shelljs")
-module.exports = function(plasma, config) {
-  plasma.on(config.reactOn, function(c, next){
-    if(!c.commands) c.commands = []
+var fs = require('fs')
+var path = require("path")
 
+module.exports = function(plasma, config) {
+
+  var constructChemicalFromCmd = function(value) {
+    var argv = value.split(" ")
+    var chemical = {
+      type: argv.shift(),
+      value: []
+    }
+    var memoKey = null
+    for(var i = 0; i<argv.length; i++) {
+      if(argv[i].indexOf("-") == 0) {
+        memoKey = argv[i].substr(1)
+        continue
+      }
+      if(memoKey) {
+        if(!chemical[memoKey])
+          chemical[memoKey] = []
+        chemical[memoKey].push(argv[i])
+      } else {
+        chemical.value.push(argv[i])
+      }
+    }
+    return chemical
+  }
+
+  var executeCommand = function(c, f, cmd, handler) {
+    console.log(cmd)
+    if(typeof cmd == "function") {
+      return cmd(c, function(r){
+        handler(r, createNext(c, f))
+      })
+    }
+    if(cmd.charAt(0) === cmd.charAt(0).toUpperCase()) {
+      var chemical = constructChemicalFromCmd(cmd)
+      return plasma.emit(chemical, function(r){
+        handler(r, createNext(c, f))
+      })
+    }
+    var child = shelljs.exec(cmd, {async: true, silent: true})
+    if(c.output)
+      child.stdout.pipe(c.output)
+    if(c.error)
+      child.stderr.pipe(c.error)
+    child.on("error", function(err){
+      handler(err, createNext(c, f))
+    })
+    child.on("close", function(code){
+      handler({code: code}, createNext(c, f))
+    })
+  }
+
+  var createNext = function(c, f){
+    return function(){
+      if(arguments.length == 3) {
+        var wrapperCommand = arguments[0]
+        var commands = c.transformCommands?c.transformCommands(arguments[1]):arguments[1]
+        if(Array.isArray(commands))
+          commands = commands.join(" && ")
+        var handler = arguments[2]
+        return createNext(c,f)(wrapperCommand+" '"+commands+"'", handler)
+      }
+
+      if(arguments.length == 2) {
+        if(typeof arguments[0] == "object" && typeof arguments[1] == "function")
+          return arguments[1](arguments[0])
+        if(typeof arguments[1] == "string" || Array.isArray(arguments[1])) {
+          var wrapperCommand = arguments[0]
+          var commands = c.transformCommands?c.transformCommands(arguments[1]):arguments[1]
+          if(Array.isArray(commands))
+            commands = commands.join(" && ")
+          return createNext(c,f)(wrapperCommand+" '"+commands+"'", createNext(c, f))
+        }
+        if(typeof arguments[1] == "function") {
+          var commands = arguments[0]
+          var handler = arguments[1]
+          if(typeof commands == "string")
+            return executeCommand(c, f, commands, handler)
+          if(Array.isArray(commands))
+            return async.eachSeries(commands, function(cmd, next){
+              executeCommand(c, f, cmd, function(r){
+                if(r.code)
+                  next()
+                else
+                  next(new Error(cmd+" failed."))
+              })
+            }, function(err){
+              handler({code: err==null?0:1, err: err}, createNext(c, f))
+            })
+        }
+      }
+
+      if(arguments.length == 1) {
+        var commands = arguments[0]
+        if(typeof commands == "string")
+          return executeCommand(c, f, commands, f)
+        if(Array.isArray(commands))
+          return async.eachSeries(commands, function(cmd, next){
+            executeCommand(c, f, cmd, function(){
+              next()
+            })
+          }, function(err){
+            f(err, createNext(c, f))
+          })
+        if(typeof commands == "object")
+          return f(commands)
+      }
+
+      if(arguments.length == 0) {
+        return f()
+      }
+    }
+  } 
+
+  var resolvePath = function(type) {
+    return path.join(process.cwd(), config.reactions, type+".js")
+  }
+  
+  plasma.on(config.reactOn, function(c, next){
+    if(!fs.existsSync(resolvePath(c.value[0]))) return false // XXX
     var f = function(r){
       if(r instanceof Error) return next && next(r)
+      if(r.code && r.code != 0) return next && next(r)
       var type = c.value.shift()
       if(type) {
-        c.type = type
-        plasma.emit(c, f)
-      } else {
-        var cmd
-        if(c.commandsWrapper)
-          cmd = c.commandsWrapper+" '"+c.commands.join(" && ")+"'"
-        else
-          cmd = c.commands.join(" && ")
-        var child = shelljs.exec(cmd, {async: true, silent: true})
-        next && next(child)
-      }
+        var reaction = require(resolvePath(type))
+        reaction(c, createNext(c, f))
+      } else
+        next && next(r)
     }
     f(c)
   })
